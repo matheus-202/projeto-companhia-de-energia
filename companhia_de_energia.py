51d65e12651823490444f003f541277bad3f60df
guilherme bohrer <guilhermebohrer2@gmail.com>
2024-10-01 14:45:42 -0300

    definicao login

--- companhia_de_energia.py
+++ companhia_de_energia.py
@@ -1,6 +1,26 @@
 print("este programa irá pedir seus dados de login ")
+# Dicionário de usuários e senhas
+usuarios = {
+    "usuario1": "senha123",
+    "usuario2": "senha456",
+    "usuario3": "senha789"
+}
+
+
+
+
 def login():
-    input("insira seu email:")
-    input("insira sua senha")
+    print("Bem-vindo ao sistema de login!")
+    
+    usuario = input("Digite seu usuário: ")
+    senha = input("Digite sua senha: ")
+    
+    # Verifica se o usuário e a senha estão corretos
+    if usuario in usuarios and usuarios[usuario] == senha:
+        print("Login bem-sucedido! Bem-vindo,", usuario)
+    else:
+        print("Usuário ou senha incorretos. Tente novamente.")
 login()
-  
+
+# Chama a função de login
+
