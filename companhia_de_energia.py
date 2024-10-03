1d81a40be9f8beece227451ab1aab9962ac241c0
guilherme bohrer <guilhermebohrer2@gmail.com>
2024-10-03 15:01:05 -0300

    editado

--- companhia_de_energia.py
+++ companhia_de_energia.py
@@ -1,34 +1,34 @@
 print("este programa irá pedir seus dados de login ")
 # Dicionário de usuários e senhas
-usuarios ={ 
-    "guilherme": "senha1234",
-    "usuario2": "senha456",
-    "usuario3": "senha789"
-}
+
 
 
 def login():
     print("Bem-vindo ao sistema de login!")
     
+usuario = input("Digite seu usuário: ")
+senha = input("Digite sua senha: ")
+
+   
+usuarios = {
+    'usuario1': 'senha123',
+    'usuario2': 'senha456',
+    'usuario3': 'senha789',
+}
+
+def login():
     usuario = input("Digite seu usuário: ")
     senha = input("Digite sua senha: ")
-    verdadeiro = True
-   
-    
-    # Verifica se o usuário e a senha estão corretos
-    if usuario in usuarios and usuarios[usuario] == senha:
-        print("Login bem-sucedido! Bem-vindo,", usuario)
-    if usuarios != senha:
-        print("incorreto")
-    if usuarios == verdadeiro:
-        print("correto")
-    
-    
+
+    if usuario not in usuarios:
+        print("Usuário não encontrado.")
+    elif usuarios[usuario] != senha:
+        print("Senha incorreta.")
+    else:
+        print("Login bem-sucedido!")
+
 login()
 
-def linhas():
-    global linhas
-    print('-' * 20)
     
 
 linhas()
